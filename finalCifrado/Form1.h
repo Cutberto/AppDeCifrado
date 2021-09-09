e30966c88572632a6063507a0b3b02cff2d7d082
cutbertonatgas <carizabalo@natgas.com.mx>
2021-09-09 09:10:05 -0500

    Mejoras en la interfaz de usuario

--- finalCifrado/Form1.h
+++ finalCifrado/Form1.h
@@ -48,12 +48,21 @@ namespace CppCLRWinformsProjekt {
 	private: System::Windows::Forms::Button^ button1;
 	private: System::Windows::Forms::TextBox^ textBox2;
 	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::Panel^ panel1;
+	private: System::Windows::Forms::Panel^ panel2;
+	private: System::Windows::Forms::Label^ label4;
+	private: System::Windows::Forms::Label^ label6;
+	private: System::Windows::Forms::Label^ label7;
+
+
+	private: System::ComponentModel::IContainer^ components;
+
 
 	private:
 		/// <summary>
 		/// Erforderliche Designervariable.
 		/// </summary>
-		System::ComponentModel::Container ^components;
+
 
 #pragma region Windows Form Designer generated code
 		/// <summary>
@@ -70,36 +79,45 @@ namespace CppCLRWinformsProjekt {
 			this->button1 = (gcnew System::Windows::Forms::Button());
 			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
 			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->panel1 = (gcnew System::Windows::Forms::Panel());
+			this->panel2 = (gcnew System::Windows::Forms::Panel());
+			this->label4 = (gcnew System::Windows::Forms::Label());
+			this->label6 = (gcnew System::Windows::Forms::Label());
+			this->label7 = (gcnew System::Windows::Forms::Label());
+			this->panel1->SuspendLayout();
+			this->panel2->SuspendLayout();
 			this->SuspendLayout();
 			// 
 			// label1
 			// 
 			this->label1->AutoSize = true;
-			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 15));
-			this->label1->Location = System::Drawing::Point(27, 27);
+			this->label1->Font = (gcnew System::Drawing::Font(L"Constantia", 15));
+			this->label1->ForeColor = System::Drawing::SystemColors::ControlLightLight;
+			this->label1->Location = System::Drawing::Point(3, 11);
 			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(246, 29);
+			this->label1->Size = System::Drawing::Size(242, 31);
 			this->label1->TabIndex = 0;
 			this->label1->Text = L"Algoritmo de cifrado.";
 			// 
 			// label2
 			// 
 			this->label2->AutoSize = true;
-			this->label2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 13));
-			this->label2->Location = System::Drawing::Point(27, 89);
+			this->label2->Font = (gcnew System::Drawing::Font(L"Constantia", 13));
+			this->label2->Location = System::Drawing::Point(322, 94);
 			this->label2->Name = L"label2";
-			this->label2->Size = System::Drawing::Size(790, 26);
+			this->label2->Size = System::Drawing::Size(425, 54);
 			this->label2->TabIndex = 1;
-			this->label2->Text = L"Introduce un mensaje en el siguiente cuadro de texto y haz clic en el botón Cifra"
-				L"r";
+			this->label2->Text = L"Introduce un mensaje en el siguiente \r\ncuadro de texto y haz clic en el botón Cif"
+				L"rar";
+			this->label2->Click += gcnew System::EventHandler(this, &Form1::label2_Click);
 			// 
 			// label5
 			// 
 			this->label5->AutoSize = true;
-			this->label5->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 15));
+			this->label5->Font = (gcnew System::Drawing::Font(L"Constantia", 15));
 			this->label5->Location = System::Drawing::Point(846, 416);
 			this->label5->Name = L"label5";
-			this->label5->Size = System::Drawing::Size(113, 29);
+			this->label5->Size = System::Drawing::Size(109, 31);
 			this->label5->TabIndex = 4;
 			this->label5->Text = L"Equipo 2";
 			this->label5->Click += gcnew System::EventHandler(this, &Form1::label5_Click);
@@ -107,27 +125,29 @@ namespace CppCLRWinformsProjekt {
 			// label3
 			// 
 			this->label3->AutoSize = true;
-			this->label3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 13));
-			this->label3->Location = System::Drawing::Point(27, 227);
+			this->label3->Font = (gcnew System::Drawing::Font(L"Constantia", 13));
+			this->label3->Location = System::Drawing::Point(322, 226);
 			this->label3->Name = L"label3";
-			this->label3->Size = System::Drawing::Size(824, 26);
+			this->label3->Size = System::Drawing::Size(458, 54);
 			this->label3->TabIndex = 5;
-			this->label3->Text = L"Introduce un mensaje en el siguiente cuadro de texto y haz clic en el botón Desci"
-				L"frar";
+			this->label3->Text = L"Introduce un mensaje en el siguiente \r\ncuadro de texto y haz clic en el botón Des"
+				L"cifrar";
 			// 
 			// textBox1
 			// 
 			this->textBox1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12));
-			this->textBox1->Location = System::Drawing::Point(32, 143);
+			this->textBox1->Location = System::Drawing::Point(327, 163);
 			this->textBox1->Name = L"textBox1";
-			this->textBox1->Size = System::Drawing::Size(778, 30);
+			this->textBox1->Size = System::Drawing::Size(483, 30);
 			this->textBox1->TabIndex = 6;
 			// 
 			// button1
 			// 
-			this->button1->BackColor = System::Drawing::SystemColors::ActiveCaption;
-			this->button1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12));
-			this->button1->Location = System::Drawing::Point(822, 140);
+			this->button1->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(10)), static_cast<System::Int32>(static_cast<System::Byte>(150)),
+				static_cast<System::Int32>(static_cast<System::Byte>(170)));
+			this->button1->Font = (gcnew System::Drawing::Font(L"Constantia", 12));
+			this->button1->ForeColor = System::Drawing::SystemColors::ControlLightLight;
+			this->button1->Location = System::Drawing::Point(822, 158);
 			this->button1->Name = L"button1";
 			this->button1->Size = System::Drawing::Size(137, 37);
 			this->button1->TabIndex = 8;
@@ -138,16 +158,17 @@ namespace CppCLRWinformsProjekt {
 			// textBox2
 			// 
 			this->textBox2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12));
-			this->textBox2->Location = System::Drawing::Point(32, 296);
+			this->textBox2->Location = System::Drawing::Point(327, 296);
 			this->textBox2->Name = L"textBox2";
-			this->textBox2->Size = System::Drawing::Size(778, 30);
+			this->textBox2->Size = System::Drawing::Size(483, 30);
 			this->textBox2->TabIndex = 9;
 			// 
 			// button2
 			// 
-			this->button2->BackColor = System::Drawing::SystemColors::ActiveCaption;
-			this->button2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12));
-			this->button2->ForeColor = System::Drawing::SystemColors::ControlText;
+			this->button2->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(10)), static_cast<System::Int32>(static_cast<System::Byte>(150)),
+				static_cast<System::Int32>(static_cast<System::Byte>(170)));
+			this->button2->Font = (gcnew System::Drawing::Font(L"Constantia", 12));
+			this->button2->ForeColor = System::Drawing::SystemColors::ControlLightLight;
 			this->button2->Location = System::Drawing::Point(822, 293);
 			this->button2->Name = L"button2";
 			this->button2->Size = System::Drawing::Size(137, 37);
@@ -156,22 +177,89 @@ namespace CppCLRWinformsProjekt {
 			this->button2->UseVisualStyleBackColor = false;
 			this->button2->Click += gcnew System::EventHandler(this, &Form1::button2_Click);
 			// 
+			// panel1
+			// 
+			this->panel1->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(10)), static_cast<System::Int32>(static_cast<System::Byte>(150)),
+				static_cast<System::Int32>(static_cast<System::Byte>(170)));
+			this->panel1->Controls->Add(this->label7);
+			this->panel1->Controls->Add(this->label6);
+			this->panel1->Controls->Add(this->label1);
+			this->panel1->Location = System::Drawing::Point(-1, -2);
+			this->panel1->Name = L"panel1";
+			this->panel1->Size = System::Drawing::Size(270, 460);
+			this->panel1->TabIndex = 11;
+			this->panel1->Paint += gcnew System::Windows::Forms::PaintEventHandler(this, &Form1::panel1_Paint);
+			// 
+			// panel2
+			// 
+			this->panel2->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(255)), static_cast<System::Int32>(static_cast<System::Byte>(255)),
+				static_cast<System::Int32>(static_cast<System::Byte>(220)));
+			this->panel2->Controls->Add(this->label4);
+			this->panel2->Location = System::Drawing::Point(268, -2);
+			this->panel2->Name = L"panel2";
+			this->panel2->Size = System::Drawing::Size(705, 76);
+			this->panel2->TabIndex = 12;
+			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->Font = (gcnew System::Drawing::Font(L"Constantia", 15));
+			this->label4->ForeColor = System::Drawing::SystemColors::ActiveCaptionText;
+			this->label4->Location = System::Drawing::Point(57, 22);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(192, 31);
+			this->label4->TabIndex = 1;
+			this->label4->Text = L"Cifrar y descifrar";
+			// 
+			// label6
+			// 
+			this->label6->AutoSize = true;
+			this->label6->Font = (gcnew System::Drawing::Font(L"Constantia", 14));
+			this->label6->ForeColor = System::Drawing::SystemColors::ButtonFace;
+			this->label6->Location = System::Drawing::Point(3, 93);
+			this->label6->Name = L"label6";
+			this->label6->Size = System::Drawing::Size(192, 29);
+			this->label6->TabIndex = 1;
+			this->label6->Text = L"Cifrar mensaje ->";
+			this->label6->Click += gcnew System::EventHandler(this, &Form1::label6_Click);
+			// 
+			// label7
+			// 
+			this->label7->AutoSize = true;
+			this->label7->Font = (gcnew System::Drawing::Font(L"Constantia", 14));
+			this->label7->ForeColor = System::Drawing::SystemColors::ButtonFace;
+			this->label7->Location = System::Drawing::Point(3, 228);
+			this->label7->Name = L"label7";
+			this->label7->Size = System::Drawing::Size(227, 29);
+			this->label7->TabIndex = 2;
+			this->label7->Text = L"Descifrar mensaje ->";
+			this->label7->Click += gcnew System::EventHandler(this, &Form1::label7_Click);
+			// 
 			// Form1
 			// 
+			this->AllowDrop = true;
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->BackColor = System::Drawing::Color::DarkTurquoise;
+			this->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(255)), static_cast<System::Int32>(static_cast<System::Byte>(255)),
+				static_cast<System::Int32>(static_cast<System::Byte>(254)));
 			this->ClientSize = System::Drawing::Size(971, 454);
+			this->Controls->Add(this->panel2);
 			this->Controls->Add(this->button2);
+			this->Controls->Add(this->label2);
 			this->Controls->Add(this->textBox2);
 			this->Controls->Add(this->button1);
 			this->Controls->Add(this->textBox1);
 			this->Controls->Add(this->label3);
 			this->Controls->Add(this->label5);
-			this->Controls->Add(this->label2);
-			this->Controls->Add(this->label1);
+			this->Controls->Add(this->panel1);
+			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::FixedSingle;
+			this->HelpButton = true;
 			this->Name = L"Form1";
 			this->Text = L"Algoritmo de cifrado. Equipo 2.";
+			this->panel1->ResumeLayout(false);
+			this->panel1->PerformLayout();
+			this->panel2->ResumeLayout(false);
+			this->panel2->PerformLayout();
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
@@ -200,6 +288,14 @@ private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e
 	textBox2->Text = gcnew String(descifradoPrincipal(nName).c_str()); ;
 
 
+}
+private: System::Void panel1_Paint(System::Object^ sender, System::Windows::Forms::PaintEventArgs^ e) {
+}
+private: System::Void label2_Click(System::Object^ sender, System::EventArgs^ e) {
+}
+private: System::Void label6_Click(System::Object^ sender, System::EventArgs^ e) {
+}
+private: System::Void label7_Click(System::Object^ sender, System::EventArgs^ e) {
 }
 };
 }
